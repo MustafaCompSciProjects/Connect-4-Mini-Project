f73c3ec77ef4d6de11c6233c4c0863decddaaef6
M.Hamade Laptop <25029104@sjd.ac.uk>
2025-11-21 12:40:57 +0000

    General improvements and feature progression

--- Connect 4 Mini Project/Program.vb
+++ Connect 4 Mini Project/Program.vb
@@ -10,8 +10,8 @@ Module Program
     Sub Main(args As String())
 
 
-
         Dim players(NUMBER_OF_PLAYERS - 1) As String
+        Dim playerSymbols(NUMBER_OF_PLAYERS - 1) As Char
 
         GetNames(players)
 
@@ -31,21 +31,52 @@ Module Program
 
         'Carry out that option
         If mainMenuChoice = 1 Then
-            PlayGame(players)
+            PlayGame(players, playerSymbols)
         Else
             End
         End If
 
     End Sub
 
-    Sub PickSymbols()
+    Sub PickSymbols(playerSymbols() As Char)
+        Dim numPlayerSymbols As Integer = playerSymbols.Length
+
+        For i As Integer = 0 To numPlayerSymbols - 1
+            Dim symbolChosen As String
+            Dim validSymbol As Boolean = False
+
+            Do
+                symbolChosen = GetCharInput($"Player {i + 1}, what symbol do you want to use")
+
+                If Not playerSymbols.Contains(symbolChosen) Then
+                    validSymbol = True
+                Else
+                    Console.WriteLine("Please enter a unique character")
+
+                End If
+            Loop Until validSymbol
+
+            playerSymbols(i) = symbolChosen
+        Next
 
     End Sub
 
-    Sub PlayGame(ByRef players() As String)
+    Sub PlayGame(ByVal players() As String, ByRef playerSymbols() As Char)
         Dim gameBoard(BOARD_WIDTH, BOARD_HEIGHT) As String
 
+        PickSymbols(playerSymbols)
 
+        InitBoard(gameBoard)
+
+        PrintBoard(gameBoard)
+
+        Dim playing As Boolean = True
+
+        While playing
+
+
+
+        End While
 
     End Sub
 
@@ -64,9 +95,18 @@ Module Program
 
     Sub PrintBoard(ByRef board(,) As String)
 
-        For i As Integer = 0 To BOARD_HEIGHT - 1
-            For j As Integer = 0 To BOARD_WIDTH - 1
+        'Setup the top row of numbers
+        Console.Write("  ")
+        For i As Integer = 1 To BOARD_WIDTH
+            Console.Write($"{i} ")
+        Next
+        Console.WriteLine()
 
+        'Show the board with its row number first
+        For i As Integer = 0 To BOARD_HEIGHT - 1
+            Console.Write($"{i + 1} ")
+
+            For j As Integer = 0 To BOARD_WIDTH - 1
                 Console.Write(board(i, j))
 
             Next
