59c1a81f5daabac2928987a5c41159ff4aa3c3b1
M.Hamade Laptop <25029104@sjd.ac.uk>
2025-11-26 19:43:11 +0000

    CheckForWin horizontal and vertical

--- Connect 4 Mini Project/Program.vb
+++ Connect 4 Mini Project/Program.vb
@@ -9,21 +9,24 @@ Module Program
     Const NUMBER_OF_PLAYERS As Integer = 2
     Const EMPTY_SLOT As String = "# "
 
+    Dim players(NUMBER_OF_PLAYERS - 1) As String
 
-    Sub Main(args As String())
+    Sub Main()
 
 
-        Dim players(NUMBER_OF_PLAYERS - 1) As String
+
         Dim playerSymbols(NUMBER_OF_PLAYERS - 1) As Char
 
-        GetNames(players)
+        If IsNothing(players(0)) Then
+            GetNames(players)
+            Console.WriteLine($"Hello {players(0)} and {players(1)}, I am the Connect 4 program")
+        End If
+        Console.Clear()
+
+
+        'System.Threading.Thread.Sleep(1000)
 
         Console.Clear()
-        Console.WriteLine($"Hello {players(0)} and {players(1)}, I am the Connect 4 program")
-
-        'System.Threading.Thread.Sleep(1000)
-
-        Console.Clear()
 
         Dim mainMenuChoice As Integer
 
@@ -34,7 +37,7 @@ Module Program
 
         'Carry out that option
         If mainMenuChoice = 1 Then
-            PlayGame(players, playerSymbols)
+            PlayGame(playerSymbols)
         Else
             End
         End If
@@ -64,7 +67,7 @@ Module Program
 
     End Sub
 
-    Sub PlayGame(ByVal players() As String, ByRef playerSymbols() As Char)
+    Sub PlayGame(ByRef playerSymbols() As Char)
         Dim gameBoard(BOARD_HEIGHT - 1, BOARD_WIDTH - 1) As String
 
         PickSymbols(playerSymbols)
@@ -125,16 +128,119 @@ Module Program
                 'If the spot below is available, then place in that spot and remove from above
                 board(i - 1, placeColumn) = symbol & " "
                 board(i, placeColumn) = EMPTY_SLOT
+                placeRow = i - 1
             End If
         Next
 
-        CheckForWin(symbol, board, placeColumn, placeRow)
+        Console.WriteLine($"Calling CheckForWin()")
+        CheckForWin(symbol, board, placeColumn, placeRow, symbolList)
 
     End Sub
 
-    Sub CheckForWin(ByVal symbol As Char, ByRef board(,) As String, ByRef recentX As Integer, ByRef recentY As Integer)
+    Sub CheckForWin(ByVal symbol As Char, ByRef board(,) As String, ByRef recentX As Integer, ByRef recentY As Integer, ByVal playerList As String)
 
         'Use the recent coordinates to check each possible direction relative to the latest move. More efficicent.
+
+        '== HORIZONTAL CHECK ==
+        Dim xToCheck As Integer
+        Dim xInARow As Integer = 1
+
+        'Slots to the right
+        For i As Integer = 1 To 3
+
+            xToCheck = recentX + i
+            If Not xToCheck < BOARD_WIDTH Then
+                Exit For
+            End If
+
+            If Not board(recentY, xToCheck) = symbol & " " Then
+                Exit For
+            End If
+
+            xInARow += 1
+
+        Next
+
+
+        'Slots to the left
+        For i As Integer = 1 To 5
+
+            xToCheck = recentX - i
+            If Not xToCheck >= 0 Then
+                Exit For
+            End If
+
+            If Not board(recentY, xToCheck) = symbol & " " Then
+                Exit For
+            End If
+
+            xInARow += 1
+
+        Next
+
+        If xInARow >= 4 Then
+            'WIN CONDITION
+            Console.WriteLine($"{players(playerList.IndexOf(symbol))} has won")
+            Thread.Sleep(1000)
+            Main()
+        End If
+
+        '== VERTICAL CHECK ==
+        Dim yToCheck As Integer
+        Dim yInARow As Integer = 1
+
+        While False
+            'Slots above 
+            For i As Integer = 1 To 3
+
+                yToCheck = recentY + i
+                If Not yToCheck < BOARD_HEIGHT Then
+                    Console.WriteLine($"Failed check within board height")
+                    Exit For
+                End If
+
+                If Not board(yToCheck, recentX) = symbol & " " Then
+                    Console.WriteLine($"Failed check {yToCheck}")
+                    Exit For
+                End If
+
+                yInARow += 1
+                Console.WriteLine($"DEBUG: Checked {yToCheck}")
+
+            Next
+        End While 'Decativate since 4 in a row has gravity
+
+        'Slots below
+        For i As Integer = 1 To 5
+
+            yToCheck = recentY - i
+            If Not yToCheck >= 0 Then
+                Console.WriteLine($"Failed check within board height")
+                Exit For
+            End If
+
+            If Not board(yToCheck, recentX) = symbol & " " Then
+                Console.WriteLine($"Failed check {yToCheck}")
+                Exit For
+            End If
+
+            yInARow += 1
+            Console.WriteLine($"DEBUG: Checked {yToCheck}")
+
+            Console.WriteLine($"DEBUG: Y in a row is {yInARow}")
+        Next
+
+        If yInARow >= 4 Then
+            'WIN CONDITION
+            Console.WriteLine($"{players(playerList.IndexOf(symbol))} has won")
+            Thread.Sleep(1000)
+            Main()
+        End If
+
+        '== Y=X ==
+
+
+
 
     End Sub
 
